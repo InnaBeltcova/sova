09c6857fc1ebdda539114382c907396842d5b90a
grace-ko <gracekodes@gmail.com>
2019-08-12 00:15:01 -0700

    started landing page

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const { port } = require('./config');
 const express = require('express');
 const app = express();
 require('./app/routes.js')(app);
-
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.listen(port, () => console.log(`The magic happens on port ${port}`))
